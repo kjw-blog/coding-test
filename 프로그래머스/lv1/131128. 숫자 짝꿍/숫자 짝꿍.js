bed10e73f250b0f0e7da4a550aa9f6947ce8ec20
jeongwook Kang <75401960+kjw-blog@users.noreply.github.com>
2023-06-27 16:36:12 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,24 @@
+function solution(X, Y) {
+    let result = {}
+    
+    for(i=0; i<10; i++) {
+        const xl = [...X].filter(item => item === `${i}`).length
+        const yl = [...Y].filter(item => item === `${i}`).length
+        
+        if(xl && yl) {
+            result[i] = Math.min(xl,yl)
+        }
+    }
+    
+    const keys = Object.keys(result).sort((a,b) => Number(b) - Number(a));
+    
+    if(!keys.length) return '-1'
+    
+    let text = keys.reduce((a,b) => a + b.repeat(result[b]), '')
+    
+    if([...text].every(item => item === '0')) {
+        text = '0'
+    }
+    
+    return  text ? text : '-1';
+}
