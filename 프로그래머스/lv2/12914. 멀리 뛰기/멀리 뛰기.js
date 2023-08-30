955941cab637293c9bb0c5062f6b0d4052dda537
jeongwook Kang <75401960+kjw-blog@users.noreply.github.com>
2023-08-30 17:18:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -7,21 +7,12 @@ function solution(n) {
         if(i<=1) {
             arr.push(i)
         } else {
-            const num = arr[i-1] + arr[i-2];
-            
-            if(arr[i-1] > flag) {
-                const p = arr.pop();
-                
-                arr.push(p%flag);
-                arr.push(num%flag)
-            } else {
-                arr.push(num);
-            }
+           arr.push((arr[i-2]+arr[i-1])%flag)
         }      
     }
     const max = arr.at(-1);
     
-    return max%1234567;
+    return max;
 }
 
 console.log(solution(1300))
